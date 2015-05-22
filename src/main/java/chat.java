c9dbada629b26deee32e9c936c7c1227ce7aeb0a
Vadiraja K <vadi.fedx@gmail.com>
2015-05-22 12:53:17 +0530

    html done

--- src/main/java/chat.java
+++ src/main/java/chat.java
@@ -9,13 +9,10 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-/*
- * a simple static http server
-*/
 public class chat {
 
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8102), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8103), 0);
         server.createContext("/test", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
@@ -23,10 +20,9 @@ public class chat {
 
     static class MyHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
-        //    String response = "Welcome Real's HowTo test page";
             Headers responseHeaders=t.getResponseHeaders();
             responseHeaders.set("Content-Type", "text/html");
-            BufferedInputStream bis=new BufferedInputStream (new FileInputStream("C:\\Html\\Chatpage.html"));
+            BufferedInputStream bis=new BufferedInputStream (new FileInputStream("C:\\Html\\home.html"));
             int len=bis.available();
             byte[] ba=new byte[len];
             bis.read(ba,0,len);
