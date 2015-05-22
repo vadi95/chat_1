769c207971eef66059bd99b0ec8b00dd7bf01dab
Vadiraja K <vadi.fedx@gmail.com>
2015-05-22 12:19:31 +0530

    html done

--- src/main/java/chat.java
+++ src/main/java/chat.java
@@ -1,84 +1,39 @@
-/**
- * Created by Vadiraja on 21-May-15.
- */
-
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-
+/*
+ * a simple static http server
+*/
 public class chat {
 
     public static void main(String[] args) throws Exception {
-
-        try {
-
-            HttpServer server = HttpServer.create(new InetSocketAddress(8097), 0);
-            server.createContext("/chat", new MyHandler());
-            server.setExecutor(null); // creates a default executor
-            server.start();
-        }
-        catch(Exception e){
-            System.out.println(e+"test");
-        }
-        finally {
-            System.out.println("Done");
-        }
-        PrintStream p=null;
-        try
-        {
-            BufferedInputStream bis=new BufferedInputStream (new FileInputStream("C:\\Html\\Chatpage.html"));
-            int len=bis.available();
-            byte[] ba=new byte[len];
-            p= new PrintStream(new FileOutputStream("C:\\Html\\error.txt"));
-            System.out.println("len= " + len);
-            bis.read(ba,0,len);
-            String response = "Welcome Real's HowTo test page";
-            //    t.sendResponseHeaders(200, response.length());
-
-
-          //  OutputStream os = t.getResponseBody();
-          //  t.sendResponseHeaders(200, len);
-            //  os.write(response.getBytes());
-            System.out.write(ba);
-            p.close();
-          //  os.close();
-        }
-        catch(Exception e){
-            p.println(e + "test");
-        }
+        HttpServer server = HttpServer.create(new InetSocketAddress(8102), 0);
+        server.createContext("/test", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
     }
 
     static class MyHandler implements HttpHandler {
-        public void handle(HttpExchange t)  {
-            PrintStream p=null;
-            try
-            {
+        public void handle(HttpExchange t) throws IOException {
+        //    String response = "Welcome Real's HowTo test page";
+            Headers responseHeaders=t.getResponseHeaders();
+            responseHeaders.set("Content-Type", "text/html");
             BufferedInputStream bis=new BufferedInputStream (new FileInputStream("C:\\Html\\Chatpage.html"));
             int len=bis.available();
-                byte[] ba=new byte[len];
-                p= new PrintStream(new FileOutputStream("C:\\Html\\error.txt"));
-                p.println("len= " + len);
+            byte[] ba=new byte[len];
             bis.read(ba,0,len);
-            String response = "Welcome Real's HowTo test page";
-        //    t.sendResponseHeaders(200, response.length());
-
-
-            OutputStream os = t.getResponseBody();
             t.sendResponseHeaders(200, len);
-            //  os.write(response.getBytes());
+            OutputStream os = t.getResponseBody();
             os.write(ba);
             os.close();
-            }
-            catch(Exception e){
-                p.println(e + "test");
-            }
         }
     }
-
-
-
 }
